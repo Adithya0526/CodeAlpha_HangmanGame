c882d6245d3e920ca8d25c6270d2ae138edc8362
Adithya0526 <sadithya.s256@gmail.com>
2025-06-25 22:12:19 +0530

    Created a basic hangman_game need to improve or make it optimized

--- HangmanGame.py
+++ HangmanGame.py
@@ -2,27 +2,32 @@ import random
 guess_elements=['omen','python','visual','game','computer']
 lives=6
 guess_word=random.choice(guess_elements)
-print('You have', lives, 'lives.')
+guess_list=list('_'*len(guess_word))  
+#print('You have', lives, 'lives.')
 print('The word to guess is:', '_' * len(guess_word))
 while lives>0:
     print('You have', lives, 'lives.')
     # print('The word to guess is:', '_' * len(guess_word))
     guess_letter=input('Guess a letter: ')
-    if guess_letter in guess_word:                            # first solution is to create a list of '_' then replace it with correct guess 
-        print('Good guess!')                                   #then check using if '_' not in the new list is so then display the correct answer and break it 
-        # Display the current state of the word 
-        for letter in guess_word:
-            if letter == guess_letter:
-                print(letter, end=' ')
-            else:
-                print('_', end=' ')
-            print()
-
-         # New line after displaying the word
-            
+    if guess_letter in guess_word:  
+        if guess_letter in guess_list:
+            print("You have already enterred this letter now choose another letter ")
+            continue                          
+        else :
+            print('Good guess!')                                  
+        # Display the current state of the word               
+        for i in range(len(guess_word)):
+            if guess_word[i] == guess_letter :
+                guess_list[i] = guess_letter
+                print(guess_list)         
+        print()
+           
     else:
         print('Wrong guess try again!')
         lives-=1
+    if '_' not in guess_list:
+        print('Congratulations! You guessed the word:', guess_word)
+        break
 if lives==0:
     print('You lost! The word was:',guess_word)
         
