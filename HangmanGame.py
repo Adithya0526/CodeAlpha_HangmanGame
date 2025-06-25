bed08deede4a83fcb74ca058113ff38f8311fb16
Adithya0526 <sadithya.s256@gmail.com>
2025-06-25 06:31:57 +0530

    I got new understanding and the game is almost completed

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,14 +1,28 @@
 import random 
 guess_elements=['omen','python','visual','game','computer']
-guess_count=0
+lives=6
 guess_word=random.choice(guess_elements)
-while guess_count<6:
+print('You have', lives, 'lives.')
+print('The word to guess is:', '_' * len(guess_word))
+while lives>0:
+    print('You have', lives, 'lives.')
+    # print('The word to guess is:', '_' * len(guess_word))
     guess_letter=input('Guess a letter: ')
-    if guess_letter in guess_word:
-        print('Correct guess you won the game!')
+    if guess_letter in guess_word:                            # first solution is to create a list of '_' then replace it with correct guess 
+        print('Good guess!')                                   #then check using if '_' not in the new list is so then display the correct answer and break it 
+        # Display the current state of the word 
+        for letter in guess_word:
+            if letter == guess_letter:
+                print(letter, end=' ')
+            else:
+                print('_', end=' ')
+            print()
+
+         # New line after displaying the word
+            
     else:
         print('Wrong guess try again!')
-        guess_count+=1
-    if guess_count==6:
-        print('You lost! The word was:',guess_word)
-        break
+        lives-=1
+if lives==0:
+    print('You lost! The word was:',guess_word)
+        
